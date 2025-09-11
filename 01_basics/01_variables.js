8479502adba1a55172a5fd17bb0a83e76b421081
MohitxSaini <160214129+MohitxSaini@users.noreply.github.com>
2025-09-11 16:51:06 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+ const accountId = 144553
+ let accountEmail = "mohit@google.com"
+ var accountPassword = "12345"
+ accountCity = "Delhi"
+ let accountState;
+
+ accountEmail = "mht@gmail.com"
+ accountPassword = "54321"
+accountCity = "Jaipur"
+ console.log(accountId);
+
+console.table({accountId, accountEmail, accountPassword, accountCity, accountState})
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+// console.log (3
+    // +3) dont write code this like 
+
+
+console.log("mohit")
+
+
+let name = "Mohit"
+let age = "26"
+let isLoggenIn = "false"
+let state;
+
+
+
+//datatypes
+
+// ** all the other non important points hsould be comment out otherwise code will show error and will print the code 
+
+
+//primitive datatypes
+
+// number => 2 to power 53
+// Bigint is used in trading, facebook, redddit for larger parts
+// string is preferred to be used in double quotes ("")
+// boolean = true/false (only two values)
+// null (standalone value) (it is a type) (null is a representation of empty value) (null is an object)
+// undefines => (variable declared but value not declared)
+// symbol (for defining uniqueness) (it is also used for figma )
+
+//object
+
+console.log(typeof undefined); output + undefined
+console.log(typeof null); // object (output)

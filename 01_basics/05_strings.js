c9fdd246c0b8dcde947a2716ad269beda1c738f9
MohitxSaini <160214129+MohitxSaini@users.noreply.github.com>
2025-09-19 15:36:05 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+// strings can be denoted by single/double quote
+
+const name = "Mohit"
+const  repoCount = 50
+
+console.log(name + repoCount + " Value");
+
+// String Interpolation or modern way of using strings following: 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}` );
+
+
+
